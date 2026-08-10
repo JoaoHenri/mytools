53293a65f240a17f494820560fd673004bfac26a
JoaoHenri <al.ferreira.henrique@impatech.edu.br>
2026-08-10 15:21:00 -0300

    Atualiza arquivos do projeto

---
+++ aulaprática01.py
@@ -0,0 +1,5 @@
+import MYTOOLS
+
+print(f'a razão entre o neperiano e pi é: {MYTOOLS.E_INT/MYTOOLS.PI_INT:02f}')
+print(f'Pi aproximado por 10 casas: {MYTOOLS.pi_real(10)}')
+print(f'Neperiano aproximado por 10 casas: {MYTOOLS.e_real(10)}')
